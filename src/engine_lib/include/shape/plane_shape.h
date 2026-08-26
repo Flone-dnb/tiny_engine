9146d44daf2511191c102d50ea4b5e028925f953
Alexander Tretyakov <flonednb@gmail.com>
2026-08-26 19:55:46 +0300

    Add plane_shape_ray_intersection.

--- src/engine_lib/include/shape/plane_shape.h
+++ src/engine_lib/include/shape/plane_shape.h
@@ -24,3 +24,28 @@ plane_shape_test_point(te_plane_shape* shape, vec3 point) {
     // Source: Real-time collision detection, Christer Ericson (2005).
     return glm_dot(shape->normal, point) - shape->distance < 0.0f;
 }
+
+// Returns `false` if intersection was not found.
+static inline bool
+plane_shape_ray_intersection(te_plane_shape* shape, vec3 ray_origin, vec3 ray, vec3 out_pos) {
+    float d = glm_vec3_dot(shape->normal, ray);
+    if (fabsf(d) < 0.0001f) {
+        return false;
+    }
+
+    vec3 temp;
+    glm_vec3_scale(shape->normal, shape->distance, temp);
+
+    glm_vec3_sub(temp, ray_origin, temp);
+    float t = glm_vec3_dot(temp, shape->normal) / d;
+    if (t < 0.0f) {
+        return false;
+    }
+
+    glm_vec3_copy(ray, temp);
+    glm_vec3_scale(temp, t, temp);
+
+    glm_vec3_copy(ray_origin, out_pos);
+    glm_vec3_add(out_pos, temp, out_pos);
+    return true;
+}
